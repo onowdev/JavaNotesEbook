82a58849f69a339d9daf1a5a8e7d356c77e15b43
onowdev <onowdev@gmail.com>
2021-12-28 15:43:07 +0700

    teest Numeric,, masih error

---
+++ src/BasicNumericPromotion.java
@@ -0,0 +1,15 @@
+public class BasicNumericPromotion {
+    public static void main(String[] args) {
+        System.out.println(int1);
+    }
+
+    static void testNumericPromotion(){
+
+        char char1 = 7 , char2 = 9;
+        short short1 = 6, short2 = 8;
+        int int1 = 5, int2 = 7;
+        float float1 = 8, float2 = 7;
+
+        int1 = char1 + char2;
+    }
+}
