e4e5aace1b410ab3673c9c41d867c41623975abd
onowdev <onowdev@gmail.com>
2021-12-29 14:20:39 +0700

    JavaBlockStatement

---
+++ src/JavaBlock.java
@@ -0,0 +1,10 @@
+public class JavaBlock {
+    public static void main(String[] args) {
+        String band = "Dream Theatre";
+
+        if(band == "Dream Theatre") { // Awal Block Statement nya
+            System.out.println("Under");
+            System.out.println("Glass Moon");
+        } // akhir Block nya
+    }
+}
