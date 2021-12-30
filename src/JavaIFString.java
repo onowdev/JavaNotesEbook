eb30e441246920966cfcd94cb6f4dc486713c992
onowdev <onowdev@gmail.com>
2021-12-30 15:00:31 +0700

    JavaIf String

---
+++ src/JavaIFString.java
@@ -0,0 +1,11 @@
+public class JavaIFString {
+    public static void main(String[] args) {
+        //Membuat String Variable
+        String language = "Java";
+
+        //If Statement
+        if (language == "Java"){
+            System.out.println("Strong Programming Language");
+        }
+    }
+}
