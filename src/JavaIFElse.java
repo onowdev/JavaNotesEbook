459cf232661884025737735f1cc0e6ae1db935a9
onowdev <onowdev@gmail.com>
2021-12-30 15:04:49 +0700

    JavaIfelse

---
+++ src/JavaIFElse.java
@@ -0,0 +1,16 @@
+public class JavaIFElse {
+    public static void main(String[] args) {
+        int number = 80;
+
+        //Check if number is greater than 0
+        if(number > 0){
+            System.out.println("Number is positive");
+        }
+        // execute this block
+        // if number not greater than 0
+        else {
+            System.out.println("The Number is Positive.");
+        }
+        System.out.println("The Number is outside block");
+    }
+}
