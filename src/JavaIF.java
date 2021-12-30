d7162c14c3a97d87d845f200d143d5549268f8e5
onowdev <onowdev@gmail.com>
2021-12-30 14:57:43 +0700

    JavaIf Statement

---
+++ src/JavaIF.java
@@ -0,0 +1,12 @@
+public class JavaIF {
+    public static void main(String[] args) {
+        int number = 100;
+
+        //Checks if number is less than 0
+
+        if (number < 0){
+            System.out.println("Number is Negative");
+        }
+        System.out.println("Statement outside if Block");
+    }
+}
