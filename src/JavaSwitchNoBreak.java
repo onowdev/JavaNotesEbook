7b4d1cfaf4f7a48ac0fc3a33fc274240e3a6077a
onowdev <onowdev@gmail.com>
2022-01-03 22:42:06 +0700

    Switch Java no break

---
+++ src/JavaSwitchNoBreak.java
@@ -0,0 +1,20 @@
+public class JavaSwitchNoBreak {
+    public static void main(String[] args) {
+        int expression= 2;
+
+        // switch statement to check size
+
+        switch (expression){
+            case 1:
+                System.out.println("case 1");
+            case 2:
+                System.out.println("case 2");
+            case 3:
+                System.out.println("Case 3");
+
+            default:
+                System.out.println("default case");
+
+        }
+    }
+}
