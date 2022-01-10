d26f7fec141e0a8f56c1f282913446fccc9154e6
onowdev <onowdev@gmail.com>
2022-01-10 22:14:01 +0700

    class welcome

---
+++ src/Welcome1.java
@@ -0,0 +1,7 @@
+public class Welcome1 {
+    // Main Method Begin
+    public static void main(String[] args) {
+        System.out.println("Welcome To My Java");
+    } // AKhir Method Main
+
+} // Akhir Class Welcome
