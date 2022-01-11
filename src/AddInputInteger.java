711b486827f5838a8b2513f1ac8a4ff2a20b93c2
onowdev <onowdev@gmail.com>
2022-01-11 21:08:04 +0700

    sum input

---
+++ src/AddInputInteger.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class AddInputInteger {
+
+    public static void main(String[] args) {
+        // Create a Scanner to obtain input from the command Window
+        Scanner input = new Scanner(System.in);
+
+        int number1; // First integer to add
+        int number2; // Second integer to add
+
+        int sum = 0;// sum of number1 and number2
+
+        System.out.println("Masukan ANgka pertama");
+        number1 = input.nextInt();
+
+        System.out.println("masukan Angka Kedua");
+        number2 = input.nextInt();
+
+
+        System.out.printf("Sum is %d%n", sum);
+    }
+}
