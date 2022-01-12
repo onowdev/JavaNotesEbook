56b8f206fcbae2a66b456e792872ee9203a96927
onowdev <onowdev@gmail.com>
2022-01-12 10:19:38 +0700

    Account class

---
+++ src/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+    private String name; // Instance Variable
+
+    // Method to set the name in the object
+    public void setName(String name)
+    {
+        this.name = name; // Store the name
+    }
+
+    //method to retrieve the name from the object
+    public String getName()
+    {
+        return name;
+    }
+} // End Class Account
