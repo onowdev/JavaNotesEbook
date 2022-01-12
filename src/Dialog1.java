2a1d5368f3ef4e511c624de3d8ce8ecd4556434c
onowdev <onowdev@gmail.com>
2022-01-12 11:53:04 +0700

    open dialog

---
+++ src/Dialog1.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Dialog1 {
+    public static void main(String[] args) {
+        //Display dialog with Message
+        JOptionPane.showMessageDialog(null, "welcome to Java");
+    }
+}
