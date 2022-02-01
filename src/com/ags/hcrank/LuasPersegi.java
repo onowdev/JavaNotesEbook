c45141512db012f2e92ce27aa3bb57f3057cd448
onowdev <onowdev@gmail.com>
2022-02-01 22:16:43 +0700

    luas persegi

---
+++ src/com/ags/hcrank/LuasPersegi.java
@@ -0,0 +1,14 @@
+package com.ags.hcrank;
+
+public class LuasPersegi {
+    public static void main (String[] args){
+        int a = 10;
+        int b = 90;
+
+        int luas;
+
+        luas = a * b;
+
+        System.out.println("Luas persegi adalah:  " + luas);
+    }
+}
