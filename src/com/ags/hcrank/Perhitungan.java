acdbd89a3b48963da2bcc55364fed0978bc7a152
onowdev <onowdev@gmail.com>
2022-02-01 22:14:17 +0700

    perhitungan sederhana

---
+++ src/com/ags/hcrank/Perhitungan.java
@@ -0,0 +1,13 @@
+package com.ags.hcrank;
+
+public class Perhitungan {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+        int c = 7, d = 9;
+        int e;
+        e = a + b + c + d;
+
+        System.out.println("Hasil = " + e);
+    }
+}
