a735f3c5bd8740bdb165a3c1166e32fe1987e8fb
onowdev <onowdev@gmail.com>
2022-02-02 11:55:34 +0700

    luas link

---
+++ src/com/ags/hcrank/LuasLink.java
@@ -0,0 +1,12 @@
+package com.ags.hcrank;
+
+public class LuasLink {
+    public static void main(String[] args) {
+        double phi = 3.14;
+        double radius = 90;
+
+        double luas = phi * radius * radius;
+
+        System.out.println("Luas Linkaran adalah : " + luas);
+    }
+}
