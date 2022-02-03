d8be17f41d16f9ce70421f6bbd49e62b4ce64034
onowdev <onowdev@gmail.com>
2022-02-03 11:18:45 +0700

    Plus Minus

---
+++ src/gforgeek/PlusMinus.java
@@ -0,0 +1,18 @@
+package gforgeek;
+
+public class PlusMinus {
+    public static void main(String[] args) {
+        byte a =126;
+
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+    }
+}
