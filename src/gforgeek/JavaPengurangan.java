4a9dbe9671559ff585e6963e8bb41d881ad6eda1
onowdev <onowdev@gmail.com>
2022-02-03 12:40:05 +0700

    oprtr pengurangan

---
+++ src/gforgeek/JavaPengurangan.java
@@ -0,0 +1,22 @@
+package gforgeek;
+
+// Java code to illustrate Subtraction operator
+
+import java.io.*;
+
+class JavaPengurangan {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 90, num2 = 10, sub = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // subtracting num1 and num2
+        sub = num1 - num2;
+        System.out.println("Subtraction = " + sub);
+    }
+}
+
