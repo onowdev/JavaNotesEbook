b7d8d02bff83b802614097a6e31d0e8721c15289
onowdev <onowdev@gmail.com>
2022-02-03 11:16:08 +0700

    Boolean

---
+++ src/gforgeek/Bulean.java
@@ -0,0 +1,9 @@
+package gforgeek;
+
+public class Bulean {
+    public static void main(String[] args) {
+        boolean b = true;
+        if (b == true)
+            System.out.println("Hi... Geek");
+    }
+}
