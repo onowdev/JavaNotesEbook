dc6eee6c6b5d3268b22f5bba6074d9e20e613495
onowdev <onowdev@gmail.com>
2022-02-03 11:51:33 +0700

    Java operator 2

---
+++ src/gforgeek/OperatorKedua.java
@@ -0,0 +1,21 @@
+package gforgeek;
+
+public class OperatorKedua {
+    public static void main(String[] args)
+    {
+
+        int x = 5, y = 8;
+
+        // concatenates x and y as
+        // first x is added to "concatenation (x+y) = "
+        // producing "concatenation (x+y) = 5"
+        // and then 8 is further concatenated.
+        System.out.println("Concatenation (x+y)= "
+                + x + y);
+
+        // addition of x and y
+        System.out.println("Addition (x+y) = "
+                + (x + y));
+    }
+}
+
