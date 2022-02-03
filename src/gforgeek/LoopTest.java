a150fba2a690d6b897d6c3fad6608e12a5a962e8
onowdev <onowdev@gmail.com>
2022-02-03 11:00:44 +0700

    loop

---
+++ src/gforgeek/LoopTest.java
@@ -0,0 +1,11 @@
+package gforgeek;
+
+public class LoopTest {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 4; x++){
+            System.out.println(x);
+        }
+        System.out.println(x);
+    }
+}
