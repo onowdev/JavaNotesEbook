a1e1ba9c0d4062a31731d4582e26ea8f728dfa14
onowdev <onowdev@gmail.com>
2022-02-03 12:48:09 +0700

    oprtr Pembagian

---
+++ src/gforgeek/JavaPembagian.java
@@ -0,0 +1,21 @@
+package gforgeek;
+
+// Java code to illustrate Division operator
+
+import java.io.*;
+
+class JavaPembagian {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 20000, num2 = 10, div = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Dividing num1 and num2
+        div = num1 / num2;
+        System.out.println("Division = " + div);
+    }
+}
