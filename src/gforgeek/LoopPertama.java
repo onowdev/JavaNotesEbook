8645eb08e842cc77f26050473efde68808e91d0b
onowdev <onowdev@gmail.com>
2022-02-03 11:14:27 +0700

    loop pertama

---
+++ src/gforgeek/LoopPertama.java
@@ -0,0 +1,11 @@
+package gforgeek;
+
+public class LoopPertama {
+    public static void main(String[] args) {
+        for (int i=1; i <=10; i++){
+            System.out.println(i);
+        }
+        int i = 15;
+        System.out.println(i);
+    }
+}
