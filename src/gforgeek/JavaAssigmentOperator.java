74f980fa5dcc746fb4e02491561ea1f6d6f838e2
onowdev <onowdev@gmail.com>
2022-02-04 09:05:17 +0700

    Assigment Operator

---
+++ src/gforgeek/JavaAssigmentOperator.java
@@ -0,0 +1,22 @@
+package gforgeek;
+
+// Java code to illustrate "=" operator
+
+import java.io.*;
+
+class JavaAssigmentOperator {
+    public static void main(String[] args)
+    {
+        // Declaring variables
+        int num;
+        String name;
+
+        // Assigning values
+        num = 100;
+        name = "Agus Sugiono";
+
+        // Displaying the assigned values
+        System.out.println("num is assigned: " + num);
+        System.out.println("name is assigned: " + name);
+    }
+}
